9ddbc2357df6fe622cf77acdefc9576af78841d5
karthik <desinghkar@gmail.com>
2017-03-14 20:46:48 -0400

    UPDATE: get3Dfeaturesfrom2D.m

--- matlab_ws/get3Dfeaturesfrom2D.m
+++ matlab_ws/get3Dfeaturesfrom2D.m
@@ -1,3 +1,20 @@
-function [x, y, z] = get3Dfeaturesfrom2D(image, list_pixels)
+function list_3d_points = get3Dfeaturesfrom2D(pcd_file, list_pixels)
+% This code is going to take the [list_pixels] which has collection of 2D
+% features of image and use the corresponding point cloud file to get the
+% 3D locations of the features
+
+% IMPORTANT: We are using organized point cloud for this function to work
+
+
+end
+
+function [x, y, z] = get3DPoint(i, j, pcd_file)
+% pcd_file is ranging from 1:307200
+point_cloud_data = readPCDFile(pcd_file);
+
+end
+
+function pc = readPCDFile(pcd_file)
+% Read the point cloud data
 
 end
