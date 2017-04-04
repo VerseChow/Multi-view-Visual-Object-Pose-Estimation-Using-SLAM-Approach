0eb98aa75a8bf7bd720d078fd934f9abca4a8518
vlnguyen92 <nguyenvanlinh1992@gmail.com>
2017-04-03 20:30:33 -0400

    New version of extract_features

--- matlab_ws/extract_features.m
+++ matlab_ws/extract_features.m
@@ -17,7 +17,7 @@ start = 1;
 feats = {};
 % folder = 'data';
 
-while start < length(images)
+while start <= length(images)
     curr_img = imread(fullfile(folder,images{start}));
     I_curr = single(rgb2gray(curr_img));
     [f_curr, d_curr] = vl_sift(I_curr);
@@ -29,12 +29,12 @@ while start < length(images)
         h = vl_plotframe(f_curr(:,perm(1:50)));
 
         hold on;    
-        next_img = imread(fullfile('data',images{i+1}));
+        next_img = imread(fullfile(folder,images{i+1}));
         I_next = single(rgb2gray(next_img));
         % match the two feature arrays
         [f_next, d_next] = vl_sift(I_next);
         [matches,scores] = vl_ubcmatch(d_curr,d_next);
-
+        matches
         feats{end+1} = matches;
         f_curr = f_next;
         d_curr = d_next;
